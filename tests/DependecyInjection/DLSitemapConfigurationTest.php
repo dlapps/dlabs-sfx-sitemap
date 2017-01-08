0f816ab4c490e29d636af155539e6fc473a4fad8
Petre Pătrașc <petre@dreamlabs.ro>
2017-01-09 00:36:30 +0200

    Add tests for the service definition and semantic configuration classes

---
+++ tests/DependecyInjection/DLSitemapConfigurationTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+namespace DL\SitemapBundle\Tests\DependecyInjection;
+
+use DL\SitemapBundle\DependencyInjection\DLSitemapConfiguration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\ArrayNode;
+use Symfony\Component\Config\Definition\ScalarNode;
+
+/**
+ * Test the behaviour of the sitemap configuration DI class.
+ *
+ * @package DL\SitemapBundle\Tests\DependecyInjection
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class DLSitemapConfigurationTest extends TestCase
+{
+    /**
+     * @var DLSitemapConfiguration
+     */
+    protected $sitemapConfiguration;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->sitemapConfiguration = new DLSitemapConfiguration;
+    }
+
+    public function testGivenThatTheTreeIsGeneratedThenASetOfSemanticConfigurationEntriesWillBeCustomisable(): void
+    {
+        /** @var ArrayNode $tree */
+        $tree     = $this->sitemapConfiguration->getConfigTreeBuilder()->buildTree();
+        $children = $tree->getChildren();
+
+        $this->assertArrayHasKey('location_prefix', $children);
+    }
+
+    public function testGivenThatTheTreeIsGeneratedThenTheLocationPrefixWillContainADefaultEmptyValue(): void
+    {
+        /** @var ArrayNode $tree */
+        $tree     = $this->sitemapConfiguration->getConfigTreeBuilder()->buildTree();
+        $children = $tree->getChildren();
+
+        /** @var ScalarNode $locationPrefix */
+        $locationPrefix = $children['location_prefix'];
+        $this->assertInstanceOf(ScalarNode::class, $locationPrefix);
+        $this->assertEquals('', $locationPrefix->getDefaultValue());
+    }
+}
---
+++ tests/DependecyInjection/DLSitemapExtensionTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace DL\SitemapBundle\Tests\DependecyInjection;
+
+use DL\SitemapBundle\DependencyInjection\DLSitemapExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Test the behaviour of the sitemap DI extensions.
+ *
+ * @package DL\SitemapBundle\Tests\DependecyInjection
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class DLSitemapExtensionTest extends TestCase
+{
+    /**
+     * @var DLSitemapExtension
+     */
+    protected $extension;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $containerBuilder;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->containerBuilder = $this->getMockBuilder(ContainerBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->extension = new DLSitemapExtension;
+    }
+
+    public function testGivenThatTheExtensionIsLoadedThenTheLocationPrefixWillBeInjectedFromTheSemanticConfiguration(): void
+    {
+        $configs = [
+            'dl_sitemap' => [
+                'location_prefix' => 'test_value',
+            ]
+        ];
+
+        $this->containerBuilder
+            ->expects($this->once())
+            ->method('setParameter')
+            ->with('sitemap_location_prefix', $configs['dl_sitemap']['location_prefix']);
+
+        $this->extension->load($configs, $this->containerBuilder);
+    }
+}
